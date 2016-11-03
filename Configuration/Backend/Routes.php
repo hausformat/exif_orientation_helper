c01856518b4a2c608daa84d4c6917d3512e3efcb
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-11-03 22:54:40 +0100

    Rename route key

--- Configuration/Backend/Routes.php
+++ Configuration/Backend/Routes.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'exif_orientation_button' => [
+    'exif_orientation_helper' => [
         'path' => '/wizard/exif_orientation',
         'target' => \Bash\ExifOrientationHelper\Controller\ExifOrientationController::class . '::mainAction'
     ]
