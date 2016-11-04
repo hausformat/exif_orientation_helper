c0915be6cd6799803d770854e3c1e5f2ef9a03a5
Ruben Schmidmeister <ruben@hausformat.com>
2016-11-04 12:15:45 +0100

    Add metadata to ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,4 +1,15 @@
 <?php
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'EXIF Orientation Helper'
+$EM_CONF[ $_EXTKEY ] = [
+    'title'       => 'EXIF Orientation Helper',
+    'description' => 'A simple button to correct the orientation of an image',
+    'version' => '0.0.1',
+    'state' => 'beta',
+    'category' => 'be',
+    'author' => 'Ruben Schmidmeister',
+    'author_email' => 'ruben.schmidmeister@icloud.com',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-8.9.99'
+        ]
+    ]
 ];
